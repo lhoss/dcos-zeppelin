4aa0758d873539c4fb07fe9b9eb6b32468a70841
Laurent Hoss <laurent.hoss@gmail.com>
2016-09-15 17:09:08 +0200

    improved Dockerfile; upgraded to latest zeppelin 0.6.1 release; added spark-2 ; flexible default user

--- docker/zeppelin-env.sh
+++ docker/zeppelin-env.sh
@@ -8,11 +8,26 @@
 [ -n "${SPARK_CORES_MAX}" ] &&
     export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.cores.max=${SPARK_CORES_MAX}"
 
+# following could be helpful
+[ -n "${SPARK_MESOS_EXECUTOR_HOME}" ] &&
+    export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.mesos.executor.home=${SPARK_MESOS_EXECUTOR_HOME}"
+
+[ -n "${SPARK_HOME}" ] &&
+    export SPARK_HOME="${SPARK_HOME}"
+
 if [ -n "${SPARK_DISPATCHER}" ]; then
     export MASTER=${SPARK_DISPATCHER}
-    export SPARK_SUBMIT_OPTIONS="--deploy-mode cluster"
+    export SPARK_SUBMIT_OPTIONS="${SPARK_SUBMIT_OPTIONS} --deploy-mode cluster"
 else
-    export MASTER=mesos://leader.mesos:5050
+    export MASTER=${MASTER:-mesos://leader.mesos:5050}
 fi
 
-export MESOS_NATIVE_LIBRARY=/usr/lib/libmesos.so
+export MESOS_NATIVE_LIBRARY="/usr/lib/libmesos.so"
+export MESOS_NATIVE_JAVA_LIBRARY="/usr/lib/libmesos.so"
+
+echo "set MASTER=$MASTER"
+echo "set MESOS_NATIVE_LIBRARY=$MESOS_NATIVE_LIBRARY"
+echo "set SPARK_SUBMIT_OPTIONS=$SPARK_SUBMIT_OPTIONS"
+echo "set SPARK_HOME=$SPARK_HOME"
+echo "set ZEPPELIN_JAVA_OPTS=$ZEPPELIN_JAVA_OPTS"
+echo "set ZEP_USER:ZEP_GROUP=$ZEP_USER:$ZEP_GROUP"
